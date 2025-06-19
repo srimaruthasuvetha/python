77d552f0be5994547ce7d00ea2a810e46b60a12a
srimaruthasuvetha <146619013+srimaruthasuvetha@users.noreply.github.com>
2025-06-19 19:54:34 +0530

    Student average checker.py

--- ifelse exercise 4.py
+++ ifelse exercise 4.py
@@ -1,12 +1,12 @@
-tamil=int(input("Enter your mark in tamil:"))
+tamil=int(input("Enter your mark in Tamil:"))
 english=int(input("Enter your mark in English:"))
 maths=int(input("Enter your mark in Maths:"))
 science=int(input("Enter your mark in Science:"))
 social=int(input("Enter your mark in Social:"))
-f=(tamil+english+maths+science+social)
-g=(f/5)
-print("Your average score is",g)
-if(g<35):
+total=(tamil+english+maths+science+social)
+average=(total/5)
+print("Your average score is",average)
+if(average<35):
     print("Additional class is required")
 else:
     print("You are good to go, Keep it up!")
